7a360c5d84a335ee31673043fe5a8743696ddb5d
KimiWaDare <abbes.alexandre@gmail.com>
2023-01-18 16:43:30 +0100

    First_project 1.0.1 organise component

--- index.js
+++ index.js
@@ -1,55 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
 
-/**
-Challenge: 
-
-- Add an `ul` inside the Header's `nav` and create
-  the following `li`s: "Pricing", "About", & "Contact"
-- Using flexbox, line up the nav items horizontally, and
-  put them inline with the React logo.
-- Change the image styling to happen in CSS instead of in-line
-  For practice, add a new class to the image in order to style it
-*/
-
-function Header() {
-    return (
-        <header>
-            <nav className="nav">
-                <img src="./react-logo.png" className="nav-logo" />
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function Footer() {
-    return (
-        <footer className="footer">
-            <small>© 2023 Abbes development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-function MainContent() {
-    return (
-        <div className="main">
-            <h2>Reasons I'm excited to learn React</h2>
-            <ol className="main-item">
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-        </div>
-    )
-}
-
-function Page() {
+function App() {
     return (
         <div className="body">
             <Header />
@@ -59,4 +14,4 @@ function Page() {
     )
 }
 
-ReactDOM.render(<Page />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
